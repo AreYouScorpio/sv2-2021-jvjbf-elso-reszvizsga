5ec391a127b3a9da0227dfc1062e6712480c7eee
AreYouScorpio <akos.schweger@gmail.com>
2021-11-09 18:02:20 +0100

    all done

--- src/main/java/cars/CarShopMain.java
+++ src/main/java/cars/CarShopMain.java
@@ -37,7 +37,7 @@ public static void main(String[] args) {
 
         System.out.println(carShop.carsWithBrand("Toyota").size()); //2
 
-        // System.out.println(carShop.carsWithBrand("BMW").size()); //0
+        System.out.println(carShop.carsWithBrand("BMW").size()); //0
 
 
 

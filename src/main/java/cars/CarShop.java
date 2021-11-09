62110343514000aaa7cae38377c6c8139f322e44
AreYouScorpio <akos.schweger@gmail.com>
2021-11-09 16:43:45 +0100

    car working on it..

--- src/main/java/cars/CarShop.java
+++ src/main/java/cars/CarShop.java
@@ -9,4 +9,25 @@ public class CarShop {
     List<Car> cars = new ArrayList<>();
 
 
+    public String getTraderName() {
+        return traderName;
+    }
+
+    public int getMaxPrice() {
+        return maxPrice;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public CarShop(String traderName, int maxPrice, List<Car> cars) {
+        this.traderName = traderName;
+        this.maxPrice = maxPrice;
+        this.cars = cars;
+
+
+
+
+    }
 }
